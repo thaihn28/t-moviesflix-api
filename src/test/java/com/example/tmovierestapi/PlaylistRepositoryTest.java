0defa309a914b673afeab5c307070fb4e975ad6a
thaihn2801 <hoangthai.solem2801@gmail.com>
2022-12-07 19:34:48 +0700

    fix: refactor payment service

--- src/test/java/com/example/tmovierestapi/PlaylistRepositoryTest.java
+++ src/test/java/com/example/tmovierestapi/PlaylistRepositoryTest.java
@@ -1,61 +1,61 @@
-package com.example.tmovierestapi;
-
-import com.example.tmovierestapi.model.Playlist;
-import com.example.tmovierestapi.payload.dto.PlaylistDTO;
-import com.example.tmovierestapi.payload.response.PagedResponse;
-import com.example.tmovierestapi.repository.PlaylistRepository;
-import com.example.tmovierestapi.service.IPlaylistService;
-import com.example.tmovierestapi.service.impl.PlaylistServiceImpl;
-import com.example.tmovierestapi.utils.AppConstants;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Rollback(value = false)
-public class PlaylistRepositoryTest {
-    @Autowired
-    private PlaylistRepository playlistRepository;
-
-    @Autowired
-    private IPlaylistService iPlaylistService;
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    @Test
-    public void testAddPlaylist(){
-//        PlaylistDTO playlistDTO = new PlaylistDTO();
-//        playlistDTO.setThumbURL("asdgsfgsfdg");
-//        playlistDTO.setName("Là Em");
-//        playlistDTO.setOriginName("Bad Romeo");
-//        playlistDTO.setSlug("la-em");
-//        playlistDTO.setYear(2022);
+//package com.example.tmovierestapi;
 //
-//        PlaylistDTO playlistDTO2 = new PlaylistDTO();
-//        playlistDTO2.setName("Là Em 2");
-//        playlistDTO2.setOriginName("Bad Romeo 2");
-//        playlistDTO2.setSlug("la-em 2");
-//        playlistDTO2.setYear(2023);
-
-
-//        playlistService.addPlaylist(playlistDTO);
-//        PlaylistDTO playlistDTO4 = iPlaylistService.addPlaylist(playlistDTO);
+//import com.example.tmovierestapi.model.Playlist;
+//import com.example.tmovierestapi.payload.dto.PlaylistDTO;
+//import com.example.tmovierestapi.payload.response.PagedResponse;
+//import com.example.tmovierestapi.repository.PlaylistRepository;
+//import com.example.tmovierestapi.service.IPlaylistService;
+//import com.example.tmovierestapi.service.impl.PlaylistServiceImpl;
+//import com.example.tmovierestapi.utils.AppConstants;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.test.annotation.Rollback;
 //
-//        PagedResponse<Playlist> playlistPagedResponse = iPlaylistService.getAllPlaylists(AppConstants.DEFAULT_PAGE_NUMBER, 30, AppConstants.SORT_DIRECTION, AppConstants.SORT_BY);
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@Rollback(value = false)
+//public class PlaylistRepositoryTest {
+//    @Autowired
+//    private PlaylistRepository playlistRepository;
 //
-//        for(Playlist playlist : playlistPagedResponse.getContent()){
-//            System.out.println(playlist.getSlug() + "adsfags");
-//        }
-
-//        Playlist savePlaylist = playlistRepository.save(playlist);
+//    @Autowired
+//    private IPlaylistService iPlaylistService;
 //
-//        Assertions.assertNotNull(playlistDTO4);
-//        Assertions.assertEquals( 1, savePlaylist.getId());
-    }
-}
+//    @Autowired
+//    private ModelMapper modelMapper;
+//
+//    @Test
+//    public void testAddPlaylist(){
+////        PlaylistDTO playlistDTO = new PlaylistDTO();
+////        playlistDTO.setThumbURL("asdgsfgsfdg");
+////        playlistDTO.setName("Là Em");
+////        playlistDTO.setOriginName("Bad Romeo");
+////        playlistDTO.setSlug("la-em");
+////        playlistDTO.setYear(2022);
+////
+////        PlaylistDTO playlistDTO2 = new PlaylistDTO();
+////        playlistDTO2.setName("Là Em 2");
+////        playlistDTO2.setOriginName("Bad Romeo 2");
+////        playlistDTO2.setSlug("la-em 2");
+////        playlistDTO2.setYear(2023);
+//
+//
+////        playlistService.addPlaylist(playlistDTO);
+////        PlaylistDTO playlistDTO4 = iPlaylistService.addPlaylist(playlistDTO);
+////
+////        PagedResponse<Playlist> playlistPagedResponse = iPlaylistService.getAllPlaylists(AppConstants.DEFAULT_PAGE_NUMBER, 30, AppConstants.SORT_DIRECTION, AppConstants.SORT_BY);
+////
+////        for(Playlist playlist : playlistPagedResponse.getContent()){
+////            System.out.println(playlist.getSlug() + "adsfags");
+////        }
+//
+////        Playlist savePlaylist = playlistRepository.save(playlist);
+////
+////        Assertions.assertNotNull(playlistDTO4);
+////        Assertions.assertEquals( 1, savePlaylist.getId());
+//    }
+//}
